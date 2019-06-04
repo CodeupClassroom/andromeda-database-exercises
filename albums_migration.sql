5aba19f8178019420f77d829d68a4d642a2eb682
Daniel Fryar <daniel.fryar@gmail.com>
2019-06-04 12:14:37 -0500

    Revert "I don't know what we changed but it all goes together"
    This reverts commit 1a16b6153794304737ca0b68cd31c159845d19cb.

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,7 +3,7 @@ USE codeup_test_db;
 drop table if exists albums;
 
 create  table albums (
-  id int UNSIGNED not null auto_increment,
+  id int unsigned not null auto_increment,
   artist varchar(255) not null,
   name varchar(255) not null,
   release_date int not null,
